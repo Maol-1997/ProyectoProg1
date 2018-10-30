d3e7349d98fe33b6e757a89b23aabec64b1d75b6
Martin Ortiz <42623358+Maol-1997@users.noreply.github.com>
2018-10-30 18:28:47 -0300

    obtener usuario json

--- api.php
+++ api.php
@@ -1,20 +1,37 @@
 <?php
+include("myconnection.php");
 if(!empty($_POST['user']) && !empty($_POST['pass'])) { // GENERAR TOKEN
-    include("myconnection.php");
     $user = $_POST['user']; // usuario
     $pass = hash('sha256', strtoupper($_POST['user'] . $_POST['pass'])); // clave
     $token = hash('sha256', strtoupper($_POST['user'] . $_POST['pass'] . date('Y-m-d H:i:s'))); // token generado
-    $params = array('usuario' => $user, 'clave' => $pass);
-    $sql = "SELECT * FROM usuario WHERE usuario = :usuario AND clave = :clave";
+    $sql = "SELECT * FROM usuario WHERE usuario = '".$user."' AND clave = '".$pass."'";
     $ejecucionSQL = $conexion->prepare($sql);
-    $ejecucionSQL->execute($params);
+    $ejecucionSQL->execute();
     $res = $ejecucionSQL->fetchAll();
     $count = $ejecucionSQL->rowCount();
     if ($count > 0) {
-        $sql = "UPDATE `programacion1`.`usuario` SET `token` = '".$token."' WHERE `usuario` = '".$user."' AND `clave` = '".$pass."'; ";
+        $sql = "UPDATE `programacion1`.`usuario` SET `token` = '" . $token . "' WHERE `usuario` = '" . $user . "' AND `clave` = '" . $pass . "'; ";
         $ejecucionSQL = $conexion->prepare($sql);
         $ejecucionSQL->execute();
-        echo "El token generado es: ".$token;
-    exit;
+        echo $token;
+    }
+}
+
+if(!empty($_POST['id'])) { // OBTENER USUARIO JSON
+    $sql = "SELECT * FROM usuario WHERE id = ".$_POST['id'];
+    $ejecucionSQL = $conexion->prepare($sql);
+    $ejecucionSQL->execute();
+    $res = $ejecucionSQL->fetchAll();
+    $count = $ejecucionSQL->rowCount();
+    foreach ($res as $rs) {
+        if ($count > 0) {
+            $json=new \stdClass();
+            $json->id=$rs['id'];
+            $json->user=$rs['usuario'];
+            $json->pass=$rs['clave'];
+            $json->token=$rs['token'];
+            echo json_encode($json);
+            break;
+        }
     }
 }
