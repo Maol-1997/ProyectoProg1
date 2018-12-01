f3a6352c2cac4d0436d6b33504b33a467931d216
Martin Ortiz <42623358+Maol-1997@users.noreply.github.com>
2018-12-01 17:39:05 -0300

    correccion

--- borrarchofer.php
+++ borrarchofer.php
@@ -1,7 +1,7 @@
 <?php
 include("myconnection.php");
-$data = json_decode(file_get_contents("php://input"));
+$data = json_decode(file_get_contents("php://input"),true);
 
-$sql = "DELETE FROM `transporte`.`chofer` WHERE `chofer_id` = '".$data->chofer_id."';";
+$sql = "DELETE FROM `transporte`.`chofer` WHERE `chofer_id` = '".$data["chofer_id"]."';";
 $ejecucionSQL = $conexion->prepare($sql);
 $ejecucionSQL->execute();
--- editarchofer.php
+++ editarchofer.php
@@ -1,39 +1,39 @@
 <?php
 include("myconnection.php");
-$data = json_decode(file_get_contents("php://input"));
+$data = json_decode(file_get_contents("php://input"),true);
 
-if(isset($data->apellido)) {
-    $sql = "UPDATE `transporte`.`chofer` SET `apellido` = '" . $data->apellido . "' WHERE `chofer_id` = '" . $data->chofer_id . "';";
+if(isset($data["apellido"])) {
+    $sql = "UPDATE `transporte`.`chofer` SET `apellido` = '" . $data["apellido"] . "' WHERE `chofer_id` = '" . $data["chofer_id"] . "';";
     $ejecucionSQL = $conexion->prepare($sql);
     $ejecucionSQL->execute();
 }
 
 if(isset($data->nombre)) {
-    $sql = "UPDATE `transporte`.`chofer` SET `nombre` = '" . $data->nombre . "' WHERE `chofer_id` = '" . $data->chofer_id . "';";
+    $sql = "UPDATE `transporte`.`chofer` SET `nombre` = '" . $data["nombre"] . "' WHERE `chofer_id` = '" . $data["chofer_id"] . "';";
     $ejecucionSQL = $conexion->prepare($sql);
     $ejecucionSQL->execute();
 }
 
 if(isset($data->documento)) {
-    $sql = "UPDATE `transporte`.`chofer` SET `documento` = '" . $data->documento . "' WHERE `chofer_id` = '" . $data->chofer_id . "';";
+    $sql = "UPDATE `transporte`.`chofer` SET `documento` = '" . $data["documento"] . "' WHERE `chofer_id` = '" . $data["chofer_id"] . "';";
     $ejecucionSQL = $conexion->prepare($sql);
     $ejecucionSQL->execute();
 }
 
 if(isset($data->email)) {
-    $sql = "UPDATE `transporte`.`chofer` SET `email` = '" . $data->email . "' WHERE `chofer_id` = '" . $data->chofer_id . "';";
+    $sql = "UPDATE `transporte`.`chofer` SET `email` = '" . $data["email"] . "' WHERE `chofer_id` = '" . $data["chofer_id"] . "';";
     $ejecucionSQL = $conexion->prepare($sql);
     $ejecucionSQL->execute();
 }
 
 if(isset($data->vehiculo_id)) {
-    $sql = "UPDATE `transporte`.`chofer` SET `vehiculo_id` = '" . $data->vehiculo_id . "' WHERE `chofer_id` = '" . $data->chofer_id . "';";
+    $sql = "UPDATE `transporte`.`chofer` SET `vehiculo_id` = '" . $data["vehiculo_id"] . "' WHERE `chofer_id` = '" . $data["chofer_id"] . "';";
     $ejecucionSQL = $conexion->prepare($sql);
     $ejecucionSQL->execute();
 }
 
 if(isset($data->sistema_id)) {
-    $sql = "UPDATE `transporte`.`chofer` SET `sistema_id` = '" . $data->sistema_id . "' WHERE `chofer_id` = '" . $data->chofer_id . "';";
+    $sql = "UPDATE `transporte`.`chofer` SET `sistema_id` = '" . $data["sistema_id"] . "' WHERE `chofer_id` = '" . $data["chofer_id"] . "';";
     $ejecucionSQL = $conexion->prepare($sql);
     $ejecucionSQL->execute();
 }
