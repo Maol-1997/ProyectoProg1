10e2c4693e137d8346043b779a8f1a656469da63
Martin Ortiz <42623358+Maol-1997@users.noreply.github.com>
2018-12-01 18:41:37 -0300

    correccion

--- agregarchofer.php
+++ agregarchofer.php
@@ -4,8 +4,6 @@ include("token.php");
 $data = json_decode(file_get_contents("php://input"),true);
 if(token($data["token"]) != false) {
     $time_pre = microtime(true);
-
-<<<<<<< HEAD:agregarchofer.php
     $sql = "INSERT INTO `transporte`.`chofer` (`apellido`, `nombre`, `documento`, `email`, `vehiculo_id`, `sistema_id`) VALUES ('" . $data["apellido"] . "', '" . $data["nombre"] . "', '" . $data["documento"] . "', '" . $data["email"] . "', '" . $data["vehiculo_id"] . "', '" . $data["sistema_id"] . "');";
     $ejecucionSQL = $conexion->prepare($sql);
     $ejecucionSQL->execute();
@@ -13,12 +11,11 @@ if(token($data["token"]) != false) {
     $time_post = microtime(true);
     $time = $time_post - $time_pre;
 
-    $sql = "INSERT INTO `transporte`.`auditoria` (`fecha_acceso`, `user`, `response_time`, `endpoint`) VALUES ('".date('Y-m-d H:i:s')."', '".token($data["token"])."', '".$time."', 'asd');";
+    $sql = "INSERT INTO `transporte`.`auditoria` (`fecha_acceso`, `user`, `response_time`, `endpoint`) VALUES ('" . date('Y-m-d H:i:s') . "', '" . token($data["token"]) . "', '" . $time . "', 'asd');";
     $ejecucionSQL = $conexion->prepare($sql);
     $ejecucionSQL->execute();
-    }
-=======
-$sql = "INSERT INTO `transporte`.`chofer` (`apellido`, `nombre`, `documento`, `email`, `vehiculo_id`, `sistema_id`) VALUES ('" . $data["apellido"] . "', '" . $data["nombre"] . "', '" . $data["documento"] . "', '" . $data["email"] . "', '" . $data["vehiculo_id"] . "', '" . $data["sistema_id"] . "');";
-$ejecucionSQL = $conexion->prepare($sql);
-$ejecucionSQL->execute();
->>>>>>> a83a6ce346fe9db25e6bd1f2c7ba99a8266055ba:agregarChofer.php
+
+    $sql = "INSERT INTO `transporte`.`chofer` (`apellido`, `nombre`, `documento`, `email`, `vehiculo_id`, `sistema_id`) VALUES ('" . $data["apellido"] . "', '" . $data["nombre"] . "', '" . $data["documento"] . "', '" . $data["email"] . "', '" . $data["vehiculo_id"] . "', '" . $data["sistema_id"] . "');";
+    $ejecucionSQL = $conexion->prepare($sql);
+    $ejecucionSQL->execute();
+}
--- borrarChofer.php
+++ borrarChofer.php
@@ -5,7 +5,6 @@ $data = json_decode(file_get_contents("php://input"),true);
 if(token($data["token"]) != false) {
     $time_pre = microtime(true);
 
-<<<<<<< HEAD:borrarchofer.php
     $sql = "DELETE FROM `transporte`.`chofer` WHERE `chofer_id` = '" . $data["chofer_id"] . "';";
     $ejecucionSQL = $conexion->prepare($sql);
     $ejecucionSQL->execute();
@@ -13,12 +12,12 @@ if(token($data["token"]) != false) {
     $time_post = microtime(true);
     $time = $time_post - $time_pre;
 
-    $sql = "INSERT INTO `transporte`.`auditoria` (`fecha_acceso`, `user`, `response_time`, `endpoint`) VALUES ('".date('Y-m-d H:i:s')."', '".token($data["token"])."', '".$time."', 'asd');";
+    $sql = "INSERT INTO `transporte`.`auditoria` (`fecha_acceso`, `user`, `response_time`, `endpoint`) VALUES ('" . date('Y-m-d H:i:s') . "', '" . token($data["token"]) . "', '" . $time . "', 'asd');";
+    $ejecucionSQL = $conexion->prepare($sql);
+    $ejecucionSQL->execute();
+
+
+    $sql = "DELETE FROM `transporte`.`chofer` WHERE `chofer_id` = '" . $data["chofer_id"] . "';";
     $ejecucionSQL = $conexion->prepare($sql);
     $ejecucionSQL->execute();
 }
-=======
-$sql = "DELETE FROM `transporte`.`chofer` WHERE `chofer_id` = '".$data["chofer_id"]."';";
-$ejecucionSQL = $conexion->prepare($sql);
-$ejecucionSQL->execute();
->>>>>>> a83a6ce346fe9db25e6bd1f2c7ba99a8266055ba:borrarChofer.php
