44dbc2c275369c196f226b8f6d29d328f80c49bb
Maol-1997 <42623358+Maol-1997@users.noreply.github.com>
2018-12-06 16:27:47 -0300

    Update Vehiculo.php

--- Vehiculo.php
+++ Vehiculo.php
@@ -51,31 +51,31 @@ switch ($_SERVER['REQUEST_METHOD']) {
         if(($user = token($data["token"])) != false) {
             $time_pre = microtime(true);
             if (isset($data["patente"])) {
-                $sql = "UPDATE `transporte`.`vehiculo` SET `patente` = '" . $data["patente"] . "' WHERE `vehiculo_id` = '" . $data["id"] . "';";
+                $sql = "UPDATE `transporte`.`vehiculo` SET `patente` = '" . $data["patente"] . "' WHERE `vehiculo_id` = '" . $data["vehiculo_id"] . "';";
                 $ejecucionSQL = $conexion->prepare($sql);
                 $ejecucionSQL->execute();
             }
 
             if (isset($data["anho_patente"])) {
-                $sql = "UPDATE `transporte`.`vehiculo` SET `anho_patente` = '" . $data["anho_patente"] . "' WHERE `vehiculo_id` = '" . $data["id"] . "';";
+                $sql = "UPDATE `transporte`.`vehiculo` SET `anho_patente` = '" . $data["anho_patente"] . "' WHERE `vehiculo_id` = '" . $data["vehiculo_id"] . "';";
                 $ejecucionSQL = $conexion->prepare($sql);
                 $ejecucionSQL->execute();
             }
 
             if (isset($data["anho_fabricacion"])) {
-                $sql = "UPDATE `transporte`.`vehiculo` SET `anho_fabricacion` = '" . $data["anho_fabricacion"] . "' WHERE `vehiculo_id` = '" . $data["id"] . "';";
+                $sql = "UPDATE `transporte`.`vehiculo` SET `anho_fabricacion` = '" . $data["anho_fabricacion"] . "' WHERE `vehiculo_id` = '" . $data["vehiculo_id"] . "';";
                 $ejecucionSQL = $conexion->prepare($sql);
                 $ejecucionSQL->execute();
             }
 
             if (isset($data["marca"])) {
-                $sql = "UPDATE `transporte`.`vehiculo` SET `marca` = '" . $data["marca"] . "' WHERE `vehiculo_id` = '" . $data["id"] . "';";
+                $sql = "UPDATE `transporte`.`vehiculo` SET `marca` = '" . $data["marca"] . "' WHERE `vehiculo_id` = '" . $data["vehiculo_id"] . "';";
                 $ejecucionSQL = $conexion->prepare($sql);
                 $ejecucionSQL->execute();
             }
 
             if (isset($data["modelo"])) {
-                $sql = "UPDATE `transporte`.`vehiculo` SET `modelo` = '" . $data["modelo"] . "' WHERE `vehiculo_id` = '" . $data["id"] . "';";
+                $sql = "UPDATE `transporte`.`vehiculo` SET `modelo` = '" . $data["modelo"] . "' WHERE `vehiculo_id` = '" . $data["vehiculo_id"] . "';";
                 $ejecucionSQL = $conexion->prepare($sql);
                 $ejecucionSQL->execute();
             }
@@ -90,7 +90,7 @@ switch ($_SERVER['REQUEST_METHOD']) {
         $data = json_decode(file_get_contents("php://input"), true);
         if(($user = token($data["token"])) != false) {
             $time_pre = microtime(true);
-            $sql = "DELETE FROM `transporte`.`vehiculo` WHERE `vehiculo_id` = '" . $data["id"] . "';";
+            $sql = "DELETE FROM `transporte`.`vehiculo` WHERE `vehiculo_id` = '" . $data["vehiculo_id"] . "';";
             $ejecucionSQL = $conexion->prepare($sql);
             $ejecucionSQL->execute();
             $time_post = microtime(true);
